52bec6a6ac62d2550feadc88cf3f6ebccfda96df
NathanMilburn <nathan.milburn@ispot.tv>
2023-08-10 11:09:37 -0700

    initial push of folder structuring and base files

---
+++ src/main/java/com/wiv/whatifvestments/WhatifvestmentsApplication.java
@@ -0,0 +1,13 @@
+package com.wiv.whatifvestments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatifvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatifvestmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiv/whatifvestments/WhatifvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiv.whatifvestments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatifvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
